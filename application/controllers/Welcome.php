75463d241c6897ee5eee03ccbe2793dd29f801a3
Enrico <enricoadi49@gmail.com>
2023-03-06 07:47:15 +0700

    init template

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		
+        $data['page_title'] = "Home";
+		$this->load->view('templates/header',$data);
 		$this->load->view('welcome_message');
+		$this->load->view('templates/footer',$data);
 	}
 }
