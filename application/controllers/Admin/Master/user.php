a207bc1a866b60ebb7e369ccb532d2dabe7a8288
estergbriel <esther_g20@mhs.istts.ac.id>
2023-04-30 18:14:56 +0700

    accept incoming change

--- application/controllers/Admin/Master/user.php
+++ application/controllers/Admin/Master/user.php
@@ -1,151 +1,111 @@
-<?php
-//Master Topik Admin 
-class user extends CI_Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->data['page_title'] = "Halaman Master User Admin";
+<?php 
+    class User extends CI_Controller{
 
-        $this->load->library("form_validation");
+        public function __construct()
+        {
+            parent::__construct();
+            $this->data['page_title'] = "Halaman Master User Admin";
+            $this->data['navigation'] = "Master"; 
+    
+            middleware_auth(4); //hak akses admin
+            $this->data['login'] = $this->UsersModel->getLogin();
 
-        //code ini digunakan untuk menjadi sebuah midlleware jika user mencoba akses halaman ini tanpa login
-        if ($this->UsersModel->getLogin() == null) {
-            $this->session->set_flashdata('header', 'Pesan');
-            $this->session->set_flashdata('message', 'Silahkan Login Terlebih Dahulu');
-            redirect('Auth');
+            $this->load->library("form_validation");
+    
+        }
+    
+        public function index(){
+             //halaman ini digunakan untuk menampilkan daftar user yang ada
+             $data = $this->data;
+             $data['page_title'] = "Master User";
+     
+             $users = $this->UsersModel->fetch();
+             $data['users'] = $users;   
+             loadView_Admin("admin/master/user/index", $data);
+        }
+ 
+        public function add(){
+            //controller ini digunakan untuk menampilkan halaman input user
+            $data = $this->data;
+            $data['page_title'] = "Input User";
+            $data['listDivisi'] = $this->DivisiModel->fetch(); 
+            loadView_Admin("admin/master/user/add", $data); 
         }
 
-        //jika tidak ada akses, maka redirect ke halaman dashboard berdasarkan hak aksesnya
-        $hak_akses = $this->UsersModel->getLogin()['KODE_HAK_AKSES'];
-        if ($hak_akses != 4) {
-            if ($hak_akses == '1') {
-                redirect('User/Dashboard'); //end user
+        public function detail($nomor_induk){
+            $data = $this->data;
+            $data['page_title'] = "Input User";
+            $user = $this->UsersModel->get($nomor_induk);
+            if($user == null){
+                redirectWith("Admin/Master/User","User tidak ditemukan");
+            } 
+            $data['user'] = $user;
+            $data['listDivisi'] = $this->DivisiModel->fetch(); 
+            loadView_Admin("admin/master/user/edit", $data); 
+        }
+        public function AddProcess(){
+            $nomor_induk = $this->input->post("nomor_induk");
+
+            //cek collision nomor induk
+            $user = $this->UsersModel->get($nomor_induk);
+            if($user != null){
+                redirectWith("Admin/Master/User","Nomor induk sudah digunakan");
             }
-            if ($hak_akses == '2') {
-                redirect('Manager'); //manager
-            } else {
-                redirect('GM'); //general manager
+
+            $nama = $this->input->post("nama");
+            $divisi = $this->input->post("divisi");
+            $hak_akses = $this->input->post("hak_akses");
+
+            $password = $this->input->post("nomor_induk");
+            
+            $user = new UsersModel();
+            $user->NOMOR_INDUK = $nomor_induk;
+            $user->NAMA = $nama;
+            $user->KODEDIV = $divisi;
+            $user->KODE_HAK_AKSES = $hak_akses;
+            $user->PASSWORD = $password;
+            $user->KODE_ATASAN = null;
+            $user->insert();
+
+            redirectWith("Admin/Master/User","Berhasil menambahkan user baru");
+
+        }
+
+        public function EditProcess($nomor_induk){
+            $user = $this->UsersModel->get($nomor_induk);
+            if($user == null){
+                redirectWith("Admin/Master/User","User tidak ditemukan");
+            } 
+            $nama = $this->input->post("nama");
+            $divisi = $this->input->post("divisi");
+            $hak_akses = $this->input->post("hak_akses");
+
+
+            try{
+                $user = new UsersModel();
+                $user->NOMOR_INDUK = $nomor_induk;
+                $user->NAMA = $nama;
+                $user->KODEDIV = $divisi;
+                $user->KODE_HAK_AKSES = $hak_akses; 
+                $user->update();
+                redirectWith("Admin/Master/User","Berhasil mengubah user");
+            }catch(Exception $e){
+                redirectWith("Admin/Master/User/detail/$nomor_induk","Gagal mengubah user");
             }
         }
-    }
-
-    public function index()
-    {
-        //halaman ini digunakan untuk menampilkan daftar topik yang ada
-        $data = $this->data;
-        $data['page_title'] = "Master";
-        $data['login'] = $this->UsersModel->getLogin();
-
-        $topics = $this->TopikModel->fetch();
-        $data['topics'] = $topics;
-
-        $this->load->view("templates/admin/header", $data);
-        $this->load->view("admin/master/topik/index", $data);
-        $this->load->view("templates/admin/footer", $data);
-    }
-    public function Menu()
-    {
-        //controller ini digunakan untuk menampilkan menu master topik, nantinya 
-        //dari menu ini akan diarahkan ke halaman lain sesuai dengan menu yang dipilih
-        $data = $this->data;
-        $data['page_title'] = "Master";
-        $data['login'] = $this->UsersModel->getLogin();
-
-        $this->load->view("templates/admin/header", $data);
-        $this->load->view("admin/master/topik/menu", $data);
-        $this->load->view("templates/admin/footer", $data);
-    }
-    public function Add()
-    {
-        //controller ini digunakan untuk menampilkan halaman input topik
-        $data = $this->data;
-        $data['page_title'] = "Master";
-        $data['list_divisi'] = $this->DivisiModel->fetch();
-        $data['login'] = $this->UsersModel->getLogin();
-        $this->load->view("templates/admin/header", $data);
-        $this->load->view("admin/master/topik/add", $data);
-        $this->load->view("templates/admin/footer", $data);
-    }
-    public function AddProcess()
-    {
-        //controller ini digunakan untuk memproses form tambahan topik
-        $topik = strtoupper($this->input->post("topik"));
-        $divisi = $this->input->post("divisi");
-        $nama = $this->input->post("nama");
-        $deskripsi = $this->input->post("deskripsi");
-
-        $kode = $this->TopikModel->getNewKode($topik);
-        $newTopik = new TopikModel();
-        $newTopik->KODE_TOPIK = $kode;
-        $newTopik->AU = 'T';
-        $newTopik->NAMA = $nama;
-        $newTopik->DIV_TUJUAN = $divisi;
-        $newTopik->DESKRIPSI = $deskripsi;
-        $newTopik->TOPIK = $topik;
-
-        $newTopik->insert();
-        $this->session->set_flashdata('header', 'Pesan');
-        $this->session->set_flashdata('message', 'Berhasil Menambahkan Topik ');
-        redirect('Admin/Master/Topik');
-    }
-    public function Detail($kode)
-    {
-        //controller ini digunakan untuk menampilkan halaman detail topik
-        $data = $this->data;
-        $data['page_title'] = "Master";
-        $data['list_divisi'] = $this->DivisiModel->fetch();
-        $data['login'] = $this->UsersModel->getLogin();
-
-        $topik = $this->TopikModel->get($kode);
-        $data['topic'] = $topik;
-
-        $this->load->view("templates/admin/header", $data);
-        $this->load->view("admin/master/topik/edit", $data);
-        $this->load->view("templates/admin/footer", $data);
-    }
-
-    public function EditProcess($kode)
-    {
-        //controller ini digunakan untuk memproses edit topik
-        $topik =  $this->input->post("topik");
-        $divisi = $this->input->post("divisi");
-        $nama = $this->input->post("nama");
-        $deskripsi = $this->input->post("deskripsi");
-
-        $updateTopik = new TopikModel();
-        $updateTopik->KODE_TOPIK = $kode;
-        $updateTopik->AU = 'T';
-        $updateTopik->NAMA = $nama;
-        $updateTopik->DIV_TUJUAN = $divisi;
-        $updateTopik->DESKRIPSI = $deskripsi;
-        $updateTopik->TOPIK = $topik;
-        $updateTopik->update();
-
-        $this->session->set_flashdata('header', 'Pesan');
-        $this->session->set_flashdata('message', 'Berhasil Mengubah Topik ');
-        redirect('Admin/Master/Topik');
-    }
-
-    public function DeleteProcess($kode)
-    {
-        //controller ini digunakan untuk menghapus topik
-        $topikDelete = new TopikModel();
-        $topikDelete->KODE_TOPIK = $kode;
-
-
-        $jumlahSubtopik1 = sizeof($topikDelete->fetchSubtopik1());
-        if ($jumlahSubtopik1 > 0) {
-            $this->session->set_flashdata('header', 'Pesan');
-            $this->session->set_flashdata('message', 'Gagal Menghapus Topik, Karena Topik Ini Memiliki Subtopik');
-            redirect('Admin/Master/Topik/Detail/' . $kode);
-        } else {
-
-            $topikDelete->delete();
-
-            $this->session->set_flashdata('header', 'Pesan');
-            $this->session->set_flashdata('message', 'Berhasil Menghapus Topik ');
-            redirect('Admin/Master/Topik');
+        public function DeleteProcess($nomor_induk){
+            $user = $this->UsersModel->get($nomor_induk);
+            if($user == null){
+                redirectWith("Admin/Master/User","User tidak ditemukan");
+            } 
+            try{
+                $user = new UsersModel();
+                $user->NOMOR_INDUK = $nomor_induk;
+                $user->delete();
+                redirectWith("Admin/Master/User","Berhasil menghapus user");
+            }catch(Exception $e){
+                redirectWith("Admin/Master/User/detail/$nomor_induk","Gagal menghapus user");
+            }
         }
-    }
-}
+    }   
+?>
