9edd1fc662187076c40cb81a2e193ebfecee925c
Enrico <enricoadi49@gmail.com>
2023-05-02 16:47:00 +0700

    webservice transfer komplain

--- application/controllers/API/User/Complained/Transfer.php
+++ application/controllers/API/User/Complained/Transfer.php
@@ -38,6 +38,17 @@ class Transfer extends REST_Controller
          
         $subtopics = $this->SubTopik2Model->fetch(); 
         $subtopik2 = $komplain->SUB_TOPIK2;
+ 
+        $this->response([
+            'message'=>'Fetch data berhasil',
+            'data' => [
+                'komplain' => $komplain,
+                'subtopics' => $subtopics,
+                'subtopik2' => $subtopik2
+            ],
+            'status'=> 202  
+        ], REST_Controller::HTTP_ACCEPTED); 
+        return;
     }
     
     public function index_post($ipost,$nomor_komplain)
@@ -59,7 +70,36 @@ class Transfer extends REST_Controller
             ], REST_Controller::HTTP_NOT_FOUND); 
             return;
         }
-         
+          //process transfer komplain
+          $topik = $this->post('inputTopik');
+          $subTopik1 = $this->post('inputSubtopik1');
+          $subTopik2 = $this->post('inputSubtopik2'); 
+
+          $komplain->TOPIK = $topik;
+          $komplain->SUB_TOPIK1 = $subTopik1;
+          $komplain->SUB_TOPIK2 = $subTopik2;
+          $komplain->updateTransferKomplain(); 
+          
+          $header = "Sukses melakukan transfer komplain";
+          $message = "Sistem telah mencatat anda melakukan transfer atas komplain $nomor_komplain, terima kasih.";
+
+          $template = templateEmail($header, $user->NAMA,
+          $message);
+          
+          $resultmail = send_mail($user->EMAIL, 
+          $header, $template); 
+          if($resultmail==false){ 
+              $this->response([
+                  'message'=>'Berhasil transfer komplain, namun gagal mengirim email', 
+                  'status'=> 202  
+              ], REST_Controller::HTTP_ACCEPTED); 
+              return;
+          }else{  
+              $this->response([
+                  'message'=>'Berhasil transfer komplain, silahkan cek email', 
+                  'status'=> 201
+              ], REST_Controller::HTTP_CREATED); 
+          }
 
     }
 }
