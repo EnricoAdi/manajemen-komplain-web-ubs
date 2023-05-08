3314288bef53a5030f9c96d0f1834a0b2f0b4788
estergbriel <esther_g20@mhs.istts.ac.id>
2023-05-08 10:40:21 +0700

    dashboard manager

--- application/controllers/Manager/Dashboard.php
+++ application/controllers/Manager/Dashboard.php
@@ -16,6 +16,31 @@ class Dashboard extends CI_Controller
     public function index(){ 
         $data = $this->data;
         $data['page_title'] = "Dashboard Manager";
+
+        $data['login'] = $this->UsersModel->getLogin(); 
+        $divisi = "";
+
+        $userLogin = $this->UsersModel->getLogin();
+        $divisi = $userLogin->KODEDIV;
+        $komplainUrgent = $this->KomplainAModel->loadManagerKomplainUrgent($divisi); 
+        $komplainTerkirim = $this->KomplainAModel->loadManagerKomplainTerkirim($divisi);
+        $komplainDiterima = $this->KomplainAModel->loadManagerKomplainDiterima($divisi);
+
+        if($komplainUrgent==null){
+            $komplainUrgent = "Belum ada";
+        }else if($komplainTerkirim==null)
+        {
+            $komplainTerkirim = "Belum ada";
+        }else if($komplainDiterima==null)
+        {
+            $komplainDiterima = "Belum ada";
+        }
+        
+        $data['divisi'] = $divisi;
+        $data['komplainUrgent'] = $komplainUrgent;
+        $data['komplainTerkirim'] = $komplainTerkirim;
+        $data['komplainDiterima'] = $komplainDiterima;
+
         loadView_Manager("manager/dashboard", $data);
     }
 }
