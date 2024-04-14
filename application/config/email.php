2dc5aa7d3cd88c2d00ebac043a7579880600eb00
Enrico <enricoadi49@gmail.com>
2024-04-14 10:30:20 +0700

    rm email

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,8 @@
     $config['smtp_host'] = 'smtp.gmail.com';
     $config['smtp_port'] = 587;
     $config['smtp_crypto'] = 'tls';
-    $config['smtp_user'] = 'trialmkomplainubs@gmail.com';
-    $config['smtp_pass'] = 'brmlpskbgwzwqjfj';
+    $config['smtp_user'] = '@gmail.com';
+    $config['smtp_pass'] = '';
     $config['mailtype'] = 'html';
     $config['charset'] = 'utf-8';
     $config['newline'] = "\r\n";
